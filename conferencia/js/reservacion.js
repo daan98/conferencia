9bd7b830990b85480d94ff284a8629cd9f8b885c
daan98 <daan98.da@gmail.com>
2022-01-08 03:44:22 -0600

    initial commit. project done

---
+++ conferencia/js/reservacion.js
@@ -0,0 +1,36 @@
+let nombre = document.getElementById("name");
+let last_name = document.getElementById("last_name");
+let email = document.getElementById("email");
+let divError = document.getElementById("error");
+
+//// VALIDACIÓN DE DATOS DEL COMPRADOR
+nombre.addEventListener("blur", personalInformation);
+last_name.addEventListener('blur', personalInformation);
+email.addEventListener('blur', personalInformation);
+email.addEventListener('blur', validatingEmail);
+
+function personalInformation() {
+    console.log(this);
+    if (this.value === "") {
+        divError.setAttribute("style", "display: block; text-align: center; padding: 10px; margin: 30px 30px 0 30px; background-color: #F2EB27; border: 1px solid red;");
+        divError.innerHTML = "este campo es necesario, Por favor llena el campo";
+        this.setAttribute("style", "border: 1px solid red;")
+    }
+    if (nombre.value !== "" && last_name.value !== "" && email.value !== "") {
+        divError.setAttribute("style", "display: none;");
+        this.setAttribute("style", "border: 1px solid black;")
+    } 
+}
+
+function validatingEmail() {
+    console.log('validating email');
+    const validEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    const validation = email.value.match(validEmail);
+    if (validation) {
+        divError.setAttribute("style", "display: none;");
+        email.setAttribute("style", "border: 1px solid black;");
+    } else {
+        divError.setAttribute("style", "display: block; text-align: center; padding: 10px; margin: 30px 30px 0 30px; background-color: #F2EB27; border: 1px solid red;");
+        divError.innerHTML = "Email no valido.";
+    }
+}
