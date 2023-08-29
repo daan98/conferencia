47782663088d7f8430fc5a1342fb3f4060cb3230
daan98 <daan98.da@gmail.com>
2023-08-29 16:57:12 -0600

    fix 'reservaciones' personal information fields (Nombre, Apellido, E-mail) validation message

--- js/reservacion.js
+++ js/reservacion.js
@@ -1,41 +1,62 @@
 let nombre = document.getElementById("name");
 let last_name = document.getElementById("last_name");
 let email = document.getElementById("email");
-let divError = document.getElementById("error");
+let divErrorName = document.getElementById("error-name");
+let divErrorLastName = document.getElementById("error-last-name");
+let divErrorEmail = document.getElementById("error-email");
 
 //// VALIDACIÓN DE DATOS DEL COMPRADOR
-nombre.addEventListener("blur", personalInformation);
-last_name.addEventListener("blur", personalInformation);
-email.addEventListener("blur", personalInformation);
+nombre.addEventListener("blur", personalInformationName);
+last_name.addEventListener("blur", personalInformationLastName);
+// email.addEventListener("blur", personalInformation);
 email.addEventListener("blur", validatingEmail);
 
-function personalInformation() {
-  if (this.value === "") {
-    divError.setAttribute(
+function personalInformationName(e) {
+  console.log("personalInformation: ", this, e);
+  if (nombre.value === "") {
+    divErrorName.setAttribute(
       "style",
       "display: block; text-align: center; padding: 10px; margin: 30px 30px 0 30px; background-color: #F2EB27; border: 1px solid red;"
     );
-    divError.innerHTML = "este campo es necesario, Por favor llena el campo";
+    divErrorName.innerHTML =
+      "El campo 'Nombre' es necesario, Por favor llena el campo";
     this.setAttribute("style", "border: 1px solid red;");
+    return;
   }
-  if (nombre.value !== "" && last_name.value !== "" && email.value !== "") {
-    divError.setAttribute("style", "display: none;");
-    this.setAttribute("style", "border: 1px solid black;");
+
+  divErrorName.setAttribute("style", "display: none;");
+  nombre.setAttribute("style", "border: 1px solid black;");
+}
+
+function personalInformationLastName() {
+  if (!last_name.value) {
+    divErrorLastName.setAttribute(
+      "style",
+      "display: block; text-align: center; padding: 10px; margin: 30px 30px 0 30px; background-color: #F2EB27; border: 1px solid red;"
+    );
+    divErrorLastName.innerHTML =
+      "El campo 'Apellido' es necesario, Por favor llena el campo";
+    this.setAttribute("style", "border: 1px solid red;");
+    return;
   }
+
+  divErrorLastName.setAttribute("style", "display: none;");
+  last_name.setAttribute("style", "border: 1px solid black;");
 }
 
 function validatingEmail() {
   const validEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
   const validation = email.value.match(validEmail);
   if (validation) {
-    divError.setAttribute("style", "display: none;");
+    divErrorEmail.setAttribute("style", "display: none;");
     email.setAttribute("style", "border: 1px solid black;");
   } else {
-    divError.setAttribute(
+    divErrorEmail.setAttribute(
       "style",
       "display: block; text-align: center; padding: 10px; margin: 30px 30px 0 30px; background-color: #F2EB27; border: 1px solid red;"
     );
-    divError.innerHTML = "Email no valido.";
+    divErrorEmail.innerHTML = "Por favor, escriba un email valido.";
+    email.setAttribute("style", "border: 1px solid red;");
   }
 }
 
